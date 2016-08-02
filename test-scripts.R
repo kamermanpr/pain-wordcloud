21b17ae873340806597bbf02c23ddf65f9801bc2
Peter Kamerman <peter.kamerman@gmail.com>
2016-08-02 18:49:20 +0200

    Testing download and parse code

--- test-scripts.R
+++ test-scripts.R
@@ -1,7 +1,25 @@
+# Test scripts to get PubMed data from NCBI into a usable format.
+
+############################################################
+#                                                          #
+#                      Load packages                       #
+#                                                          #
+############################################################
 library(rentrez)
-# Get PubMed search fields
+library(purrr)
+
+############################################################
+#                                                          #
+#                 Get PubMed search fields                 #
+#                                                          #
+############################################################
 entrez_db_searchable(db = 'pubmed', config = NULL)
 
+############################################################
+#                                                          #
+#                  Set search parameters                   #
+#                                                          #
+############################################################
 # NLM journal [JOUR] abbreviations: Pain (est: 1975), J Pain (est: 2000),
 # Eur J Pain (est: 1997), Clin J Pain (est: 1985)
 # Year of publication [PDAT]
@@ -9,28 +27,51 @@ entrez_db_searchable(db = 'pubmed', config = NULL)
 srch_terms <- c('((Pain[Journal] NOT Pain Suppl[Journal])
                 OR J Pain[Journal] OR Eur J Pain[Journal]
                 OR Clin J Pain[Journal])
-                AND 2015[PDAT] : 2015[PDAT]
+                AND 1975[PDAT] : 2015[PDAT]
                 AND Journal Article[PTYP]')
-# Search
-pubmed_srch <- entrez_search(db = 'pubmed',
+
+############################################################
+#                                                          #
+#                    Explore search results                #
+#                                                          #
+############################################################
+# Find out how many articles are returned by search
+explore_srch <- entrez_search(db = 'pubmed',
                              term = srch_terms,
                              retmode = 'xml',
-                             retmax = 0,
-                             use_history = TRUE)
+                             retmax = 0)
+# Use 'count' data from 'explore_srch' to set the 'retmax' in a new search
+id_srch <- entrez_search(db = 'pubmed',
+                          term = srch_terms,
+                          retmode = 'xml',
+                          retmax = explore_srch$count)
 
-# fetch
-pubmed_fetch <- list()
-for(seq_start in seq(1, 1000 , 100)) {
-    for(i in seq_along(seq(1, 1000 , 100))) {
-    pubmed_fetch[[i]] <- entrez_fetch(db = 'pubmed',
-                                web_history = pubmed_srch$web_history,
-                                rettype = 'xml',
-                                parsed = TRUE,
-                                retmax = 100,
-                                restart = seq_start)
+############################################################
+#                                                          #
+#               Get and sort publication IDs               #
+#                                                          #
+############################################################
+# Extract all publication ids into numeric vector 'ids'
+ids <- id_srch$ids
+# Define a sequence to split the vector of ids into n = 100 chunks
+splitter <- seq(from = 1, to = id_srch$count, by = 100)
+# Define an empty list of length 'splitter'
+id_list <- vector(mode = 'list', length = length(splitter))
+# For loop to split 'ids'
+for(i in seq_along(splitter)) {
+    id_list[[i]] <- ids[splitter[[i]]:(splitter[[i]] + 99)]
     }
-}
 
-
-# Parse
-pubmed_parsed <- purrr::map(.x = x, .f = parse_pubmed_xml)
+############################################################
+#                                                          #
+#                  Download full records                   #
+#                                                          #
+############################################################
+# For testing use a subsample of id_list
+id_test <- id_list[1:2]
+id_test[[1]] <- id_test[[1]][1:20]
+id_test[[2]] <- id_test[[2]][1:20]
+test_rec <- id_test %>%
+    map(entrez_fetch, db = 'pubmed', rettype = 'xml') %>%
+    map(parse_pubmed_xml) %>%
+    map(1:2)
